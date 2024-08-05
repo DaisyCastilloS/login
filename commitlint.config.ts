bc97e345ca27451562b309a8af635719282267e6
Daisy Castillo <daisyktbpa@gmail.com>
2024-08-04 20:29:10 -0400

    incorrect message format

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,8 +1,15 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-      'header-pattern': [2, 'always', /^(\w+)\((\w+)#\d+\): .+$/],
-      'subject-case': [2, 'always', 'sentence-case'],
-    },
-  };
-  
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-pattern': [2, 'always', /^(\w+)\(\w+#\d+\): .+$/],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test'
+      ]
+    ],
+    'subject-case': [2, 'always', 'sentence-case'],
+  },
+};
+sessionStorage
